ddb752145fc36f03db4019aa8d7fe0e907557677
carissa morrow <carissamorrow@hotmail.com>
2018-11-02 17:47:34 -0600

    orderchanges

--- game.js
+++ game.js
@@ -1,19 +1,11 @@
-// var titleElem = document.getElementById("title");
-// var slapsElem = document.getElementById("slap-button");
-// var kicksElem = document.getElementById("kick-button");
-// var punchElem = document.getElementById("punch-button");
-// var healthElem = document.getElementById("get health");
-// var hitsElem = document.getElementById("get-hits")
-
-
 let evilHamburger = {
   name: "mr. evil",
   attacks: {
     punch: 0,
     kicks: 0,
     slap: 0,
-    health: 100
   },
+  health: 100,
   modifiers: []
 }
 
@@ -27,22 +19,18 @@ function totalItems() {
 }
 
 function punch() {
-  evilHamburger.health = 20 + totalItems()
-  document.getElementById("health-status").innerText = evilHamburger.health.toString()
-  endGame();
+  evilHamburger.health -= 20 + totalItems()
+  update();
 }
 
 function kick() {
-  evilHamburger.health = 10 + totalItems()
-  document.getElementById("health-status").innerText = evilHamburger.health.toString()
-  endGame();
+  evilHamburger.health -= 10 + totalItems()
+  update()
 }
 
 function slap() {
-  evilHamburger.health = 20 + totalItems()
-  evilHamburger.modifiers.push(-1)
-  document.getElementById("health-status").innerText = evilHamburger.health.toString()
-  endGame();
+  evilHamburger.health -= 20 + totalItems()
+  update();
 }
 
 function tomato() {
@@ -59,7 +47,8 @@ function lettuce() {
 }
 
 function update() {
-  var healthElem = document.getElementById("health");
+  endGame();
+  document.getElementById("health-status").innerText = evilHamburger.health.toString()
 }
 
 function reset() {
