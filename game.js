3db43f0dd6ba8b4b7c505ebc121d318cb69bc5a3
carissa morrow <carissamorrow@hotmail.com>
2018-11-01 21:39:25 -0600

    additions

--- game.js
+++ game.js
@@ -1,25 +1,7 @@
+const title = document.getElementById("title");
 const slaps = document.getElementById("slap-button");
 const kicks = document.getElementById("kick-button");
 const punch = document.getElementById("punch-button");
+const healt = document.getElementById("get health");
 
 var health = 100
-
-let evilHamburger = {
-  name: "evil hamburger",
-  levels: ["normal", "moderatlely hurt", "very hurt", "morgue"],
-  levelimage: ["https://static1.fjcdn.com/comments/I+havent+had+a+mcdonalds+burger+in+at+least+3+_e83fd1ea7af4ab5f68ca0c6ef7d948d0.jpg"],
-  health: 100,
-  slap: 0,
-  tolerance: 3,
-  level: "base"
-}
-
-let evilHamburger2 = {
-  name: "evil hamburger two",
-  levels: ["normal", "moderatlely hurt", "very hurt", "morgue"],
-  levelimage: [],
-  health: 90,
-  slap: 0,
-  tolerance: 3,
-  level: "base"
-}
